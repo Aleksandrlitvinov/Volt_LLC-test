7c53f704a5417b9c81c2480cd4f2a9004cefd81e
Aleksandrlitvinov <84243297+Aleksandrlitvinov@users.noreply.github.com>
2024-03-10 12:41:14 +0100

    feat: add component Input

---
+++ src/shared/ui/input/input.tsx
@@ -0,0 +1,46 @@
+import {ChangeEvent, ComponentPropsWithoutRef, forwardRef} from 'react'
+
+import {TextField} from '@mui/material'
+import {TextFieldProps} from '@mui/material/TextField/TextField'
+import clsx from 'clsx'
+
+import s from './input.module.scss'
+
+type InputPropsType = {
+  errorInput?: string
+  label?: string
+  onValueChange?: (value: string) => void
+  type: 'text'
+  value?: string
+} & ComponentPropsWithoutRef<'input'>
+
+type GenInputType = InputPropsType & TextFieldProps
+export const Input = forwardRef<HTMLInputElement, GenInputType>((props, ref) => {
+  const {error, errorInput, label, onValueChange, placeholder, type, value, ...rest} = props
+
+  const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    if (onValueChange) {
+      onValueChange(e.target.value)
+    }
+  }
+  const classNames = {
+    input: clsx(s.input, error && s.error),
+  }
+
+  return (
+    <div className={s.inputWrapper}>
+      {label && <div className={s.label}>{label}</div>}
+      <TextField
+        className={classNames.input}
+        error={error}
+        onChange={onChangeHandler}
+        placeholder={placeholder}
+        ref={ref}
+        type={type}
+        value={value}
+        {...rest}
+      />
+      {errorInput && <div className={s.errorMessage}>{errorInput}</div>}
+    </div>
+  )
+})
